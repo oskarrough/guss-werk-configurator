d1d2ee5c41a25197ba4096afc7583aa1ba90e50d
Gregory Vespasien <evevitrugno@Airdeevvitrugno.fritz.box>
2019-06-02 18:31:01 +0200

    Implement Friday's feedback

--- configurator.js
+++ configurator.js
@@ -1,41 +1,52 @@
 const hyper = window.hyperHTML;
+const configurator = document.querySelector("clothes-configurator");
 
-function createMenuElement(feature, renderFct) {
+function getMenu(feature, renderFct) {
+
+    // get selected value for next select's first option
+    const menuClassName = "." + feature.id + " > select";
+    let select = configurator.shadowRoot.querySelector(menuClassName);
+    let selectValue =  select ? select.value : feature.options[0];
+
     return hyper(feature)`
         <div class="${`Menu ${feature.id}`}">
-            <h5
-                    class="${`Menu-title`}"
-                >
-                    ${feature.title}
-                </h5>
-                <select
-                    class="${`Menu-list`}"
-                    onchange=${renderFct}
-                >
-                    ${feature["options"].map(option => {
-                        return hyper()`
-                            <option
-                                class="Menu-list-item"
-                                value=${option}
-                            >
-                                ${option}
-                            </option>
-                        `;
-                    })}
-                <select>
+            <label
+                class="Menu-title"
+                for="Menu-list"
+            >
+                ${feature.title}
+            </label>
+            <select
+                class="Menu-list"
+                onchange=${renderFct}
+            >
+                <option value=${selectValue}>${selectValue}</option>
+                ${feature.options.filter( option => { if (selectValue) return option != selectValue; }) // avoid duplicating first option 
+                .map(option => {
+                    return hyper()`
+                        <option
+                            class="Menu-list-item"
+                            value=${option}
+                        >
+                            ${option}
+                        </option>
+                    `;
+                })}
+            <select>
         </div>
     `;
 }
 
-function createProductOptionElement(feature) {
+function getProductItem(feature) {
 
-    let select = document.querySelector(feature.id); // Does not exist yet
-    let source = `${feature.url}${select.value}${feature.format}`;
+    let select = configurator.shadowRoot.querySelector("." + feature.id + " > select");
+    let option = select ? select.value : feature.options[0];
+    let source = `${feature.url}${option}${feature.format}`;
     return hyper()`
         <div class="${`ProductItem ${feature.id}`}">
                     <img
                         class="${`Product-${feature.id}-img`}"
-                        src= ${source}
+                        src= ${option == "none" ? "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs%3D" : source}
                         alt=${option}
                     >
         </div>
@@ -49,6 +60,7 @@ export class Configurator extends HTMLElement {
         this.shadow = this.attachShadow({mode:"open"});
         this.html = hyper(this.shadow);
 
+        this.render = this.render.bind(this);
         this.render();
         this.loadStyle();
     }
@@ -60,12 +72,15 @@ export class Configurator extends HTMLElement {
     render() {
         this.html`
             <div class="Config">
-                <div class="Menus">
+                <form class="Menus">
                     <h1 class="Title">Design your custom ${this.product}</h1>
-                    ${this.model.map(feature => { return createMenuElement(feature, this.render); })}
-                </div>
+                    ${this.model.map(feature => { return getMenu(feature, this.render); })}
+                    <div class="BtnBox">
+                        <button class="BtnBox-button" type="submit">Submit your ${this.product}</button>
+                    </div>
+                </form>
                 <div class="Product">
-                    ${this.model.map(feature => { return createProductOptionElement(feature); })}
+                    ${this.model.map(feature => { return getProductItem(feature); })}
                 </div>
             </div>
         `;
