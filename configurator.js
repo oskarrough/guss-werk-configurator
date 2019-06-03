92826c0d38547f0ab77352d07ae88a11595928a3
Gregory Vespasien <evevitrugno@Airdeevvitrugno.fritz.box>
2019-06-03 20:19:46 +0200

    refactor Menus to allow features grouping

--- configurator.js
+++ configurator.js
@@ -7,56 +7,68 @@ function slugify(string) {
         .toLowerCase()
 }
 
-function getMenu(configurator, feature, renderFct) {
+function getMenu(configurator, featureArr, renderFct) {
 
-    // get selected value for next select's first option
-    const menuClassName = "." + feature.id + " > select";
-    let select = configurator.shadowRoot.querySelector(menuClassName);
-    let selectValue =  select ? select.value : feature.options[0];
+    let multipleFeaturesFlag = "";
+    if (featureArr.length > 1) multipleFeaturesFlag = "Flex";
 
-    return hyper(feature)`
-        <div class="${`Menu ${feature.id}`}">
-            <label
-                class="Menu-title"
-                for="Menu-list"
-            >
-                ${feature.title}
-            </label>
-            <select
-                class="Menu-list"
-                onchange=${renderFct}
-            >
-                ${feature.options.map(option => {
-                    return hyper()`
-                        <option
-                            class="Menu-list-item"
-                            value=${option}
-                            selected=${option === selectValue}
+    return hyper(featureArr)`
+        <div class=${`MenuWrapper ${multipleFeaturesFlag}`}>
+            ${featureArr.map(feature => {
+                // get selected value for next select's first option
+                const menuClassName = "." + feature.id + " > select";
+                let select = configurator.shadowRoot.querySelector(menuClassName);
+                let selectValue =  select ? select.value : feature.options[0];
+                return hyper()`
+                    <div class="${`Menu ${feature.id}`}">
+                        <label
+                            class="Menu-title"
+                            for="Menu-list"
                         >
-                            ${option}
-                        </option>
-                    `;
-                })}
-            <select>
+                            ${feature.title}
+                        </label>
+                        <select
+                            class="Menu-list"
+                            onchange=${renderFct}
+                        >
+                            ${feature.options.map(option => {
+                                return hyper()`
+                                    <option
+                                        class="Menu-list-item"
+                                        value=${option}
+                                        selected=${option === selectValue}
+                                    >
+                                        ${option}
+                                    </option>
+                                `;
+                            })}
+                        <select>
+                    </div>  
+                `;
+            })}
         </div>
     `;
 }
 
-function getProductItem(configurator, feature) {
+function getProductItem(configurator, featureArr) {
 
-    let select = configurator.shadowRoot.querySelector("." + feature.id + " > select");
-    let option = select ? select.value : feature.options[0];
-    let formattedOption = option.includes(" ") ? slugify(option) : option;
-    let source = `${feature.url}${formattedOption}${feature.format}`;
-    return hyper()`
-        <div class="${`ProductItem ${feature.id}`}">
-                    <img
-                        class="${`Product-${feature.id}-img`}"
-                        src= ${formattedOption == "" ? "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs%3D" : source}
-                        alt=${formattedOption}
-                    >
-        </div>
-    `;
+    return featureArr.map(feature => {
+
+        let select = configurator.shadowRoot.querySelector("." + feature.id + " > select");
+        let option = select ? select.value : feature.options[0];
+        let formattedOption = option.includes(" ") ? slugify(option) : option;
+        let source = `${feature.url}${formattedOption}${feature.format}`;
+        return hyper()`
+            <div class="${`ProductItem ${feature.id}`}">
+                        <img
+                            class="${`Product-${feature.id}-img`}"
+                            src= ${formattedOption == "" ? "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs%3D" : source}
+                            alt=${formattedOption}
+                        >
+            </div>
+        `;
+    })
+    
 }
 
 export class Configurator extends HTMLElement {
@@ -80,13 +92,13 @@ export class Configurator extends HTMLElement {
             <div class="Config">
                 <form class="Menus">
                     <h1 class="Title">Design your custom ${this.product}</h1>
-                    ${this.model.map(feature => { return getMenu(this, feature, this.render); })}
+                    ${this.model.map(featureArr => { return getMenu(this, featureArr, this.render); })}
                     <div class="BtnBox">
                         <button class="BtnBox-button" type="submit">Submit ${this.product}</button>
                     </div>
                 </form>
                 <div class="Product">
-                    ${this.model.map(feature => { return getProductItem(this, feature); })}
+                    ${this.model.map(featureArr => { return getProductItem(this, featureArr); })}
                 </div>
             </div>
         `;
--- model.js
+++ model.js
@@ -2,51 +2,56 @@
 
 export const product = "apron";
 
+// data structure is an arr of arrays of objects
 export const model = [
 
-    {
-        id: "accessoires",
-        title:"Metal accessoires",
-        url: "/assets/accessoires-",
-        format: ".png",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris in elit augue",
-        options: ["beige","schwarz","braun","grau", "rot"],
-        grouping: ""
-    },
-    {
-        id: "brust",
-        title:"Brust",
-        url: "/assets/brust-",
-        format: ".png",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris in elit augue",
-        options: ["","kleine Tasche links", "grosse Brusstasche", "kleine Tasche rechts"],
-        grouping: ""
-     },
-    {
-         id: "hüfteRechts",
-         title: "Hüftetasche Rechts",
-         url: "/assets/hufterechts-",
-         format: ".png",
-         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris in elit augue",
-         options: ["","kleine Tasche", "grosse Tasche", "Touchonhalter"],
-         grouping: "hüfteLinks"
-    },
-    {
-        id: "hüfteLinks",
-        title: "Hüftetasche Links",
-        url: "/assets/huftelinks-",
-        format: ".png",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris in elit augue",
-        options: ["", "kleine Tasche",  "grosse Tasche", "Touchonhalter"],
-        grouping: ""
-    },
-    {
-        id: "front",
-        title:"Front Länge",
-        url: "/assets/front-",
-        format: ".png",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris in elit augue",
-        options: ["lang"],
-        grouping: ""
-    }
+    [
+        {
+            id: "accessoires",
+            title:"Metallaccessoires",
+            url: "/assets/accessoires-",
+            format: ".png",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris in elit augue",
+            options: ["beige","schwarz","braun","grau", "rot"],
+        }
+    ],
+    [
+        {
+            id: "brust",
+            title:"Brust",
+            url: "/assets/brust-",
+            format: ".png",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris in elit augue",
+            options: ["","kleine Tasche links", "grosse Brusstasche", "kleine Tasche rechts"],
+        }
+    ],
+    [ 
+        {
+            id: "hüfteRechts",
+            title: "Hüftetasche Rechts",
+            url: "/assets/hufterechts-",
+            format: ".png",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris in elit augue",
+            options: ["","kleine Tasche", "grosse Tasche", "Touchonhalter"],
+        },
+
+        {
+            id: "hüfteLinks",
+            title: "Hüftetasche Links",
+            url: "/assets/huftelinks-",
+            format: ".png",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris in elit augue",
+            options: ["", "kleine Tasche",  "grosse Tasche", "Touchonhalter"],
+        }
+    ],
+    [
+        {
+            id: "front",
+            title:"Front Länge",
+            url: "/assets/front-",
+            format: ".png",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris in elit augue",
+            options: ["lang"],
+        }
+    ]
 ]
