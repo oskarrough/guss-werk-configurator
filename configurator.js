b8925d433154bf304ea2daeb9e67d488baf915b6
Gregory Vespasien <evevitrugno@Airdeevvitrugno.fritz.box>
2019-06-08 13:30:56 +0200

    fix indentation in configurator.js

--- configurator.js
+++ configurator.js
@@ -1,69 +1,72 @@
 const hyper = window.hyperHTML;
 
 function slugify(string) {
-    return string 
-        .replace(/ /g, "-")
-        .trim()
-        .toLowerCase()
+  return string 
+    .replace(/ /g, "-")
+    .trim()
+    .toLowerCase()
 }
 
 function findSelectedValueFor(configurator, feature) {
-    let select = configurator.shadowRoot.querySelector(`.${feature.id} > select`);
-    return select ? select.value : feature.options[0];
+  
+  let select = configurator.shadowRoot.querySelector(`.${feature.id} > select`);
+  return select ? select.value : feature.options[0];
+
 }
 
 function getMenu(configurator, featureArr, renderFct) {
 
-    let multipleFeaturesFlag = "";
-    if (featureArr.length > 1) multipleFeaturesFlag = "Flex";
+  let multipleFeaturesFlag = "";
+  if (featureArr.length > 1) multipleFeaturesFlag = "Flex";
 
-    return hyper(featureArr)`
-    <div class=${`MenuWrapper ${multipleFeaturesFlag}`}>
-      ${featureArr.map(feature => {
-        // get selected value for selected option
-        let selectValue = findSelectedValueFor(configurator, feature);
-        return hyper(feature)`
-          <div class="${`Menu ${feature.id}`}">
-            <label class="Menu-title" for="Menu-list" >
-              ${feature.title}
-            </label>
-            <select
-              class="Menu-list"
-              onchange=${renderFct}
-            >
-              ${feature.options.map(option => {
-                return hyper()`
-                  <option
-                    class="Menu-list-item"
-                    value=${option}
-                    selected=${option === selectValue}
-                  >
-                    ${option}
-                  </option>
-                `;
-              })}
-            </select>
-          </div>  
-        `;
-      })}
-    </div>
-  `;
+  return hyper(featureArr)`
+  <div class=${`MenuWrapper ${multipleFeaturesFlag}`}>
+    ${featureArr.map(feature => {
+      // get selected value for selected option
+      let selectValue = findSelectedValueFor(configurator, feature);
+      return hyper(feature)`
+        <div class="${`Menu ${feature.id}`}">
+          <label class="Menu-title" for="Menu-list" >
+            ${feature.title}
+          </label>
+          <select
+            class="Menu-list"
+            onchange=${renderFct}
+          >
+            ${feature.options.map(option => {
+              return hyper()`
+                <option
+                  class="Menu-list-item"
+                  value=${option}
+                  selected=${option === selectValue}
+                >
+                  ${option}
+                </option>
+              `;
+            })}
+          </select>
+        </div>  
+      `;
+    })}
+  </div>
+`;
 }
 
 function getProductItem(configurator, baseURL, featureArr) {
 
-    return featureArr.map(feature => {
-        
-        let option = findSelectedValueFor(configurator, feature);
-        option = slugify(option);
-        let source = `${baseURL}${feature.url}${option}${feature.format}`;
-        let src = option === "" ? "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs%3D" : source;
-        return hyper(feature, ':option')`
-          <div class="${`ProductItem ${feature.id}`}">
-            <img src= ${src} alt=${option} >
-          </div>
-        `;
-    })
+  return featureArr.map(feature => {
+
+    let option = findSelectedValueFor(configurator, feature);
+    option = slugify(option);
+    let source = `${baseURL}${feature.url}${option}${feature.format}`;
+    let src = option === "" ? "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs%3D" : source;
+    return hyper(feature, ':option')`
+      <div class="${`ProductItem ${feature.id}`}">
+        <img src= ${src} alt=${option} >
+      </div>
+    `;
+
+  })
 }
 
 function getColorMenu(colorsArr) {
@@ -95,45 +98,45 @@ function changeColorMask(event) {
 export class Configurator extends HTMLElement {
 
     constructor() {
-        super();
-        this.shadow = this.attachShadow({mode:"open"});
-        this.html = hyper(this.shadow);
+      super();
+      this.shadow = this.attachShadow({mode:"open"});
+      this.html = hyper(this.shadow);
 
-        this.render = this.render.bind(this);
-        this.render();
-        this.loadStyle();
+      this.render = this.render.bind(this);
+      this.render();
+      this.loadStyle();
     }
 
     set model(featuresObject) {
-        this.model = featuresObject;
+      this.model = featuresObject;
     }
   
     render() {
-        this.html`
-            <div class="Config">
-                <form class="Menus">
-                    <h1 class="Title">Design your custom ${this.model.product}</h1>
-                    ${getColorMenu(this.model.colors)}
-                    ${this.model.features.map(featureArr => getMenu(this, featureArr, this.render))}
-                    <div class="BtnBox">
-                        <button class="BtnBox-button" type="submit">Submit ${this.product}</button>
-                    </div>
-                </form>
-                <div class="Product">
-                    <img src="./assets/lsa-basis/base-mask-lang.png" class="Mask ProductItem" style=${`filter: ${this.model.colors[0].filter}`}>
-                    ${this.model.features.map(featureArr => getProductItem(this, this.model.imageFolder, featureArr))}
-                </div>
+      this.html`
+        <div class="Config">
+          <form class="Menus">
+            <h1 class="Title">Design your custom ${this.model.product}</h1>
+            ${getColorMenu(this.model.colors)}
+            ${this.model.features.map(featureArr => getMenu(this, featureArr, this.render))}
+            <div class="BtnBox">
+              <button class="BtnBox-button" type="submit">Submit ${this.product}</button>
             </div>
-        `;
+          </form>
+          <div class="Product">
+            <img src="./assets/lsa-basis/base-mask-lang.png" class="Mask ProductItem" style=${`filter: ${this.model.colors[0].filter}`}>
+            ${this.model.features.map(featureArr => getProductItem(this, this.model.imageFolder, featureArr))}
+          </div>
+        </div>
+      `;
     }
 
     loadStyle() {
-        const style = hyper()`
-                <style>
-                    @import "./styling.css"
-                </style>
-        `;
-        this.shadowRoot.appendChild(style);
+      const style = hyper()`
+        <style>
+          @import "./styling.css"
+        </style>
+      `;
+      this.shadowRoot.appendChild(style);
     }
 }
 
