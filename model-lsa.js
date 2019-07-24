d134eef0b2485209446f5dee0eb66f934f643d12
PillePiletti <52037791+PillePiletti@users.noreply.github.com>
2019-07-24 19:37:24 +0200

    Update model-lsa.js

--- model-lsa.js
+++ model-lsa.js
@@ -4,15 +4,15 @@ export default {
 
 	colors: [
 		{
+			name: 'BLAU, Twill 60%CO/40%PES, 240g/m',
+			filter: 'invert(50%) sepia(100%) saturate(211%) hue-rotate(167deg) brightness(69%) contrast(196%)'
+		},
+		{
 			name: 'hellgrau',
 			filter: 'none'
 		},
 		{
-			name: 'BLAU,Twill 60%CO/40%PES, 240g/m',
-			filter: 'invert(50%) sepia(100%) saturate(211%) hue-rotate(167deg) brightness(69%) contrast(196%)'
-		},
-		{
-			name: 'SCHWARZ<br>Canvas oder Twill 60%CO/40%PES, 320g/m',
+			name: 'SCHWARZ, Canvas oder Twill 60%CO/40%PES, 320g/m',
 			filter: 'invert(40%) sepia(10%) saturate(63%) hue-rotate(9deg) brightness(100%) contrast(211%)'
 		},
 		{
