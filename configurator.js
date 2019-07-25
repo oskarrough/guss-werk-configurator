3589f3e3d962cc1b2d3d9cce950ca8dfff12c1c0
PillePiletti <52037791+PillePiletti@users.noreply.github.com>
2019-07-25 09:38:38 +0200

    Update configurator.js

--- configurator.js
+++ configurator.js
@@ -137,7 +137,7 @@ export class Configurator extends HTMLElement {
             </div>
           </form>
           <div class="Product">
-            <img src="./assets/lsa-basis/alt-maske-lang.png" class="ProductItem Mask" style=${`filter: ${this.model.colors[0].filter}`} alt=${this.model.colors[0].name}>
+            <img src="./assets/lsa-basis/maske-lang.png" class="ProductItem Mask" style=${`filter: ${this.model.colors[0].filter}`} alt=${this.model.colors[0].name}>
             ${this.model.features.map(featureArr => getProductItem( this, this.model.imageFolder, featureArr ))}
           </div>
         </div>
