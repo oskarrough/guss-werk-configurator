71196e45ce759357bc74c420fa6b0caf98f771a2
PillePiletti <52037791+PillePiletti@users.noreply.github.com>
2024-09-29 22:51:03 +0200

    Update configurator.js

--- configurator.js
+++ configurator.js
@@ -171,8 +171,8 @@ export class Configurator extends HTMLElement {
 					</div>
 
 					<p style="margin-top: 1em">
-						<label>Anzahl <small>(Mindestbestellmenge 50 Stk)</small>
-						<input type="number" name="amount" value="50" min="50">
+						<label>Anzahl <small>(Mindestbestellmenge 12 Stk)</small>
+						<input type="number" name="amount" value="12" min="12">
 						</label>
 					</p>
 					<p>
